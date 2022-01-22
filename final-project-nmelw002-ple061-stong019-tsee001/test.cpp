7a3f3a5a1ab2fc8faeb70e03c2ef45cb60c4aced
Paulian Le <paulianle7@gmail.com>
2022-01-22 01:22:52 -0800

    Forgotten test cases

--- final-project-nmelw002-ple061-stong019-tsee001/test.cpp
+++ final-project-nmelw002-ple061-stong019-tsee001/test.cpp
@@ -14,8 +14,33 @@ TEST(FactoryTest, MCTest)
 	Question* t1 = f1.createQuestion("MC", "How many group members do you have?", "4", "1", "2", "3","");
 	
 
-        EXPECT_EQ(t1->getQuestion(),"How many group members do you have?\n4\n1\n2\n3\n"); // could we use stringify() and what output would we be expecting?
+        EXPECT_EQ(t1->getQuestion(),"How many group members do you have?\n4\n1\n2\n3\n"); 
 }
+
+TEST(FactoryTest, MCBigNumTest)
+{
+
+        // Create a factory
+        QuestionFactory f1;
+	
+	Question* t1 = f1.createQuestion("MC", "How many group members do you have?", "400000", "100000", "200000", "300000","");
+	
+
+        EXPECT_EQ(t1->getQuestion(),"How many group members do you have?\n400000\n100000\n200000\n300000\n"); 
+}
+
+TEST(FactoryTest, EmptyMCTest)
+{
+
+        // Create a factory
+        QuestionFactory f1;
+	
+	Question* t1 = f1.createQuestion("", "", "", "", "", "","");
+	
+
+        EXPECT_EQ(t1->getQuestion(),""); 
+}
+
 TEST(FactoryTest, TFTest)
 {
 
@@ -24,8 +49,21 @@ TEST(FactoryTest, TFTest)
 
         Question* t1 = f1.createQuestion("TF", "Nate is the VP of cyber@UCR", "True", "", "", "", "");
 
-        EXPECT_EQ(t1->getQuestion(),"True or False:\nNate is the VP of cyber@UCR\n"); // could we use stringify() and what output would we be expecting?
+        EXPECT_EQ(t1->getQuestion(),"True or False:\nNate is the VP of cyber@UCR\n"); 
 }
+
+TEST(FactoryTest, EmptyTFTest)
+{
+
+        // Create a factory
+        QuestionFactory f1;
+	
+	Question* t1 = f1.createQuestion("", "", "", "", "", "","");
+	
+
+        EXPECT_EQ(t1->getQuestion(),""); 
+}
+
 TEST(FactoryTest, FRQTest)
 {
 
@@ -34,8 +72,21 @@ TEST(FactoryTest, FRQTest)
 
         Question* t1 = f1.createQuestion("FRQ", "Why is it so hot in Riverside?", "", "", "", "", "");
 
-        EXPECT_EQ(t1->getQuestion(),"Why is it so hot in Riverside?\n"); // could we use stringify() and what output would we be expecting?
+        EXPECT_EQ(t1->getQuestion(),"Why is it so hot in Riverside?\n"); 
 }
+
+TEST(FactoryTest, EmptyFRQTest)
+{
+
+        // Create a factory
+        QuestionFactory f1;
+	
+	Question* t1 = f1.createQuestion("", "", "", "", "", "","");
+	
+
+        EXPECT_EQ(t1->getQuestion(),""); 
+}
+
 TEST(FactoryTest, MatchingTest)
 {
 
@@ -44,7 +95,19 @@ TEST(FactoryTest, MatchingTest)
 
         Question* t1 = f1.createQuestion("MATCHING", "Michael Phelps", "Michael Jordan", "Michael Jackson", "Swimmer", "Basketball Player", "Singer");
 
-        EXPECT_EQ(t1->getQuestion(), "Matching Question\nMichael Phelps   Swimmer\nMichael Jordan   Basketball Player\nMichael Jackson   Singer\n\n"); // could we use stringify() and what output would we be expecting?
+        EXPECT_EQ(t1->getQuestion(), "Matching Question\nMichael Phelps   Swimmer\nMichael Jordan   Basketball Player\nMichael Jackson   Singer\n\n"); 
+}
+
+TEST(FactoryTest, EmptyMatchingTest)
+{
+
+        // Create a factory
+        QuestionFactory f1;
+	
+	Question* t1 = f1.createQuestion("", "", "", "", "", "","");
+	
+
+        EXPECT_EQ(t1->getQuestion(),""); 
 }
 
 int main(int argc, char **argv) {
